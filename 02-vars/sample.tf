56d5727f5761ae09de4d9e699ef6fb3db503cce6
Azhar <azharshaikh33@gmail.com>
2024-05-16 18:27:28 +0400

    Auto Push

--- 02-vars/sample.tf
+++ 02-vars/sample.tf
@@ -6,8 +6,8 @@ output "sample_op" {
     value = var.sample
 }
 
-output "sample_out put" {
-    value = " Value of a sample variable is ${var.sample}"
+output "sample_output" {
+    value = "Value of a sample variable is ${var.sample}"
 }
 
 variable "integer" {
