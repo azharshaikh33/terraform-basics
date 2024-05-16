37dd0bcae6e2a01a34615053b6da16077d7100a1
Azhar <azharshaikh33@gmail.com>
2024-05-17 00:31:28 +0400

    Auto Push

---
+++ 03-example/ec2.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region    = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+    ami             = "ami-072983368f2a6eab5"
+    instance_type   = "t3.micro"
+
+    tags = {
+      name = "Terraform-Instance"
+    }
+}
+
+output "private_dns_of_server" {
+    value = "aws_instance.web.private_dns"
+  
+}
