59b63a3f5cbde9090a35debc1cbc531e8c6af2e4
Azhar <azharshaikh33@gmail.com>
2024-05-17 01:25:37 +0400

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -8,6 +8,7 @@ provider "aws" {
 resource "aws_instance" "web" {
     ami = "ami-072983368f2a6eab5"
     instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.allow_ssh_sg.id]
 
     tags = {
       name = "Terraform-Instance-Server"
