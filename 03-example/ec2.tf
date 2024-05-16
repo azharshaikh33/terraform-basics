7df9fa25f2f0392ed9107d5b2c4af8b62bcd63ea
Azhar <azharshaikh33@gmail.com>
2024-05-17 01:14:44 +0400

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -26,16 +26,16 @@ resource "aws_security_group" "allow_ssh_sg" {
 
   # Inbound rules
   ingress {
-    from_port   = 0
-    to_port     = 0
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Outbound rules
   egress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
