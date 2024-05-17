50b05fd09bddeec87bb91a2b97d5a898dc1d1f20
Azhar <azharshaikh33@gmail.com>
2024-05-18 01:13:21 +0400

    Auto Push

--- 04-examples/main.tf
+++ 04-examples/main.tf
@@ -3,7 +3,14 @@ module "ec2" {
     source = "./ec2"
     sg = module.sg.sg_id
 }
+# Declaring a module with name sg
 
 module "sg" {
     source = "./sg"
+}
+
+# Declaring the output from ec2 module
+
+output "private_dns" {
+  value = module.ec2.private_dns
 }
